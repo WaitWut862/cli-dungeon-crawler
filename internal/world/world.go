ad169877ff9c75b524f88979bf58bf45435f5105
Carson Morton <cgmorton862@gmail.com>
2026-02-18 19:00:44 -0500

    started chunk generation

--- internal/world/world.go
+++ internal/world/world.go
@@ -2,6 +2,7 @@ package world
 
 import (
 	c "cli-dungeon-crawler/internal/common"
+	m "cli-dungeon-crawler/internal/mobs"
 )
 
 type World struct {
@@ -30,6 +31,13 @@ func (w *World) UpdateTick() {
 	w.Tick = w.Tick + 1
 }
 
-func (w *World) GenerateChunk(pt1, pt2 c.Position) {
-	 
+func (w *World) GenerateChunk(pt1, pt2 c.Position, ground string) {
+	xRange := pt1.X - pt2.X
+	yRange := pt1.Y - pt2.Y
+	for x := range xRange {
+		for y := range yRange {
+			w.TileMap[c.Position{X: x, Y: y}] = Tile{GroundType: ground}
+		}
+	}
+
 }
